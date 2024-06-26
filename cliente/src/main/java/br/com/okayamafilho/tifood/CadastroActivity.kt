e1f77e313e981c24c127c38ac5ad91780e18b3dd
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2024-06-26 09:57:05 -0300

    Primeiro commit do projeto Tifood

---
+++ cliente/src/main/java/br/com/okayamafilho/tifood/CadastroActivity.kt
@@ -0,0 +1,26 @@
+package br.com.okayamafilho.tifood
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.okayamafilho.tifood.databinding.ActivityCadastroBinding
+import br.com.okayamafilho.tifood.databinding.ActivityMainBinding
+
+class CadastroActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityCadastroBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        inicializar()
+    }
+
+    private fun inicializar() {
+
+    }
+}
---
+++ cliente/src/main/java/br/com/okayamafilho/tifood/MainActivity.kt
@@ -0,0 +1,24 @@
+package br.com.okayamafilho.tifood
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.okayamafilho.tifood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        inicializar()
+    }
+
+    private fun inicializar() {
+
+    }
+}
